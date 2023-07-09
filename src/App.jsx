8822cde0de493d9a568d987405d64f98c8dc8d1b
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-09 11:12:02 +0100

    add initial App.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Navbar from './components/navbar'
+import Home from './pages/home.jsx'
+import About from './pages/about.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
   )
 }
 
