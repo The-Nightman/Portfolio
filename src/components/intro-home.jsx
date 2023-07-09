1a43aea584825822763a5f8f626e71b6e56dd7ba
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-09 15:54:02 +0100

    add intro-home structure

--- src/components/intro-home.jsx
+++ src/components/intro-home.jsx
@@ -1,10 +1,22 @@
 const IntroHome = () => {
-  
-    return (
-      <>
-      </>
-    )
-  }
-  
-export default IntroHome
-  
+  return (
+    <>
+      <section className="personal-intro">
+        <span className="tags">&lt;intro&gt;</span>
+        <h1 className="intro-title">
+          <span className="keenan">Keenan</span>
+          <span className="func">.io()</span>
+        </h1>
+        <p className="name">Keenan Brant</p>
+        <p>
+          Junior Software Developer
+          <br />
+          based in the UK
+        </p>
+        <span className="tags">&lt;/intro&gt;</span>
+      </section>
+    </>
+  );
+};
+
+export default IntroHome;
