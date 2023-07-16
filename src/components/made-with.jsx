534bff8c9b09999afd7fd5cd9e4999aa56b17112
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-16 19:47:11 +0100

    add made-with.jsx structure

--- src/components/made-with.jsx
+++ src/components/made-with.jsx
@@ -1,10 +1,97 @@
 const MadeWith = () => {
-  
-    return (
-      <>
-      </>
-    )
-  }
-  
-export default MadeWith
-  
+  return (
+    <>
+      <section class="made-with">
+        <h2 class="tags">/*This site was built with*/</h2>
+        <div class="made-with-grid">
+          <ul class="col-tools">
+            <h3 class="made-with-col-title">
+              <span>def</span>
+              <span class="func">(</span>
+              <span>_tools</span>
+              <span class="func">)</span>
+            </h3>
+            <li>
+              <span>// HTML</span>
+            </li>
+            <li>
+              <span>// CSS</span>
+            </li>
+            <li>
+              <span>// JS</span>
+            </li>
+            <li>
+              <span>// React</span>
+            </li>
+            <li>
+              <span>// Adobe Illustrator</span>
+            </li>
+            <li>
+              <span>// Adobe Photoshop</span>
+            </li>
+          </ul>
+          <ul class="col-music">
+            <h3 class="made-with-col-title">
+              <span>def</span>
+              <span class="func">(</span>
+              <span>_music</span>
+              <span class="func">)</span>
+            </h3>
+            <li>
+              <a href="https://open.spotify.com/track/7wdwIaXUuzlu1grzWMFRJm?si=5abfe714ff464a5b">
+                // Ghost
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/7ISL3LO8AWP3fKIXunvqTa?si=33420d9f1d94451a">
+                // Wolfmother
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/0wvIGFIgbyz4JNwQhZgTv2?si=f5645a70cd954fa3">
+                // Alice In Chains
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/3LPgJC2BDy9Ji8QBhwioI2?si=35beb7a46d4a487b">
+                // Spoon
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/5OQsiBsky2k2kDKy2bX2eT?si=526b8f04876347b9">
+                // Foo Fighters
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/6pC4bAgzuhOyLJPxsZSQxi?si=cb623072573f4a61">
+                // Temples
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/1iDcKYNvo6gglrOG6lvnHL?si=e4c0935b585e4811">
+                // The Rolling Stones
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/2AxCeJ6PSsBYiTckM0HLY7?si=dfb20acbd44241df">
+                // Jimmy Hendrix
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/47W6YR93MPCGLEUReLMyDm?si=d8b7339b381c4408">
+                // Jamiroquai
+              </a>
+            </li>
+            <li>
+              <a href="https://open.spotify.com/track/5Vx3Cb6AM8TNqQqkwIMDh4?si=89a63267534843ba">
+                // Turbowolf
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MadeWith;
