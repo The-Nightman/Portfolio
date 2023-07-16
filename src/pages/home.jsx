0651fea7ba869711eaad623f2a379bff6c0dc785
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-16 19:48:24 +0100

    add complete home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,21 @@
+import IntroHome from "../components/intro-home"
+import CurriculumVitae from "../components/curriculum-vitae"
+import SkillsHome from "../components/skills-home"
+import Projects from "../components/projects"
+import MadeWith from "../components/made-with"
+
 const Home = () => {
   
     return (
       <>
+      <IntroHome/>
+      <CurriculumVitae/>
+      <SkillsHome/>
+      <Projects/>
+      <MadeWith/>
+      <footer>
+        <p>Rev1.5 Built in 2023<br/>© 2023 Keenan Brant</p>
+      </footer>
       </>
     )
   }
