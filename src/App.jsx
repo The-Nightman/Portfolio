9beac04a83b19615b18f811978f4beb17c5c290d
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 22:49:37 +0100

    delete app.css and replace with proper root index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { TriCol, Navbar} from './components'
 import { Home, About } from './pages'
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
 
 function App() {
 
