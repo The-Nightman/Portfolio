87910137cb17ecb707829f4b10fb67782af1eeb0
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 22:22:06 +0100

    add components and pages import/export barrels

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import IntroHome from "../components/intro-home";
+import IntroAbout from "../components/intro-about";
+import CurriculumVitae from "../components/curriculum-vitae";
+import SkillsHome from "../components/skills-home";
+import Projects from "../components/projects";
+import MadeWith from "../components/made-with";
+import AboutPersonality from "./about-personality";
+import TriCol from "../components/tri-col";
+import Navbar from "../components/navbar";
+import AboutSkills from "../components/about-skills";
+
+export {
+  IntroHome,
+  IntroAbout,
+  CurriculumVitae,
+  SkillsHome,
+  Projects,
+  MadeWith,
+  AboutPersonality,
+  TriCol,
+  Navbar,
+  AboutSkills,
+};
