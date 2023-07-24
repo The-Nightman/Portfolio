217915823ca61c1e8481f099eaccd49fa6ec5ea6
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 22:31:56 +0100

    add about-skills

---
+++ src/components/about-skills.jsx
@@ -0,0 +1,40 @@
+const AboutSkills = () => {
+  return (
+    <>
+      <div className="about-skills">
+        <ul className="skills-grid">
+          <h2 className="about-skills-title">Skills</h2>
+          <li>
+            <span>// HTML</span>
+          </li>
+          <li>
+            <span>// CSS</span>
+          </li>
+          <li>
+            <span>// JS</span>
+          </li>
+          <li>
+            <span>// Python</span>
+          </li>
+          <li>
+            <span>// Adobe Illustrator</span>
+          </li>
+          <li>
+            <span>// Adobe Photoshop</span>
+          </li>
+          <li>
+            <span>// Adobe Xd</span>
+          </li>
+          <li>
+            <span>// Figma</span>
+          </li>
+          <li>
+            <span>// SVG</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default AboutSkills;
