4fbeca600e368bdd5ab089ffd469a5036b902375
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 22:31:00 +0100

    add intro-about

--- src/components/intro-about.jsx
+++ src/components/intro-about.jsx
@@ -1,10 +1,19 @@
 const IntroAbout = () => {
-  
-    return (
-      <>
-      </>
-    )
-  }
-  
-export default IntroAbout
-  
+  return (
+    <>
+      <div className="about-personal">
+        <h1 className="about-title">
+          <p className="name">Keenan Brant</p>
+        </h1>
+        <p>
+          Junior Software Developer
+          <br />
+          based in the UK
+        </p>
+        <p>// Music and Gym addict, concept art fan and video game fiend</p>
+      </div>
+    </>
+  );
+};
+
+export default IntroAbout;
