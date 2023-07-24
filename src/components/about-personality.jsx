b21ada76a9c304a9d7ef81dfbe0d185224c5ff0a
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 22:32:39 +0100

    add about-personality

---
+++ src/components/about-personality.jsx
@@ -0,0 +1,22 @@
+const AboutPersonality = () => {
+  return (
+    <>
+      <div className="about-also">
+        <ul className="also-grid">
+          <h2 className="about-also-title">Also getting on with</h2>
+          <li>
+            <span>// Gym</span>
+          </li>
+          <li>
+            <span>// Music</span>
+          </li>
+          <li>
+            <span>// Games</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default AboutPersonality;
