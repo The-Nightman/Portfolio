067a95c9b115e72e96bc3573348a07bb82520d94
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-24 23:36:03 +0100

    fix about page classname missnaming

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,13 +4,13 @@ const About = () => {
   
     return (
       <>
-      <span className="tags">&lt;container&gt;</span>
+      <span className="container-tags">&lt;container&gt;</span>
       <span className="tags">&lt;About&gt;</span>
       <IntroAbout/>
       <AboutSkills/>
       <AboutPersonality/>
       <span className="tags">&lt;/About&gt;</span>
-      <span className="tags">&lt;/container&gt;</span>
+      <span className="container-tags">&lt;/container&gt;</span>
       <MadeWith/>
       </>
     )
