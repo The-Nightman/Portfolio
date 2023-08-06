3d78960726e38033cb2606ffff4b2bbb9f497711
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-06 20:29:19 +0100

    add project card jsx and add dynamic project cards

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,7 +1,26 @@
-const ProjectCard = () => {
-    return (
-        <div></div>
-    )
-}
+import { ReactComponent as GithubSVG } from "../assets/github.svg";
+
+const ProjectCard = ({ image, name, body, repo, live }) => {
+  return (
+    <div className="proj-card">
+      <img src={image} width="250" height="250" />
+      <p className="description">
+        {name}
+        <br />
+        <br />
+        {body}
+      </p>
+      <a
+        type="button"
+        href={repo}
+        target="_blank"
+        className="proj-1-button"
+      >
+        <GithubSVG className="project-github" />
+        <span>Repository</span>
+      </a>
+    </div>
+  );
+};
 
 export default ProjectCard;
