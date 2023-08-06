8a7a53e819d102c2e046e5d7ecd4ceb27ef0c9a8
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-06 19:58:10 +0100

    add div for styling

--- src/components/skills-home.jsx
+++ src/components/skills-home.jsx
@@ -18,29 +18,31 @@ const SkillsHome = () => {
           <span className="func">)</span>
         </h2>
         <span>/*view more skills in about*/</span>
-        <ul>
-          <li>
-            <HtmlSVG className="skills-svg" />
-          </li>
-          <li>
-            <CssSVG className="skills-svg" />
-          </li>
-          <li>
-            <JavaScriptSVG className="skills-svg" />
-          </li>
-          <li>
-            <ReactSVG className="skills-svg" />
-          </li>
-          <li>
-            <NodeSVG className="skills-svg" />
-          </li>
-          <li>
-            <ExpressSVG className="skills-svg" />
-          </li>
-          <li>
-            <PsqlSVG className="skills-svg" />
-          </li>
-        </ul>
+        <div className="skills-svg-container">
+          <ul>
+            <li>
+              <HtmlSVG className="skills-svg" />
+            </li>
+            <li>
+              <CssSVG className="skills-svg" />
+            </li>
+            <li>
+              <JavaScriptSVG className="skills-svg" />
+            </li>
+            <li>
+              <ReactSVG className="skills-svg" />
+            </li>
+            <li>
+              <NodeSVG className="skills-svg" />
+            </li>
+            <li>
+              <ExpressSVG className="skills-svg" />
+            </li>
+            <li>
+              <PsqlSVG className="skills-svg" />
+            </li>
+          </ul>
+        </div>
         <span className="tags">&lt;/skills&gt;</span>
       </section>
     </>
