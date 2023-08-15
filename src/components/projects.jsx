aee489dd2bd57ad9592b658356523f640f852bab
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-15 20:48:09 +0100

    add framer motion carousel slider

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,16 @@
 import { ProjectCard } from "./";
 import { projects } from "../data/projects";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
 
 const Projects = () => {
+  const [width, setWidth] = useState(0)
+  const carousel = useRef()
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <>
       <section className="projects">
@@ -9,23 +18,27 @@ const Projects = () => {
         <h2 className="projects-title">
           <span>projects</span>
           <span className="func">&#123;</span>
-          <span>display</span>
+          <span>slider</span>
           <span className="func">&#125;</span>
         </h2>
-        <div className="projects-cards">
+        <motion.div ref={carousel} className="projects-carousel-container" whileTap={{cursor: "grabbing"}}>
+          <motion.div drag="x" dragConstraints={{right:0, left: -width}} className="projects-cards">
             {projects.map(({ image, name, body, repo, live }) => {
               return (
-                <ProjectCard
-                  key={name}
-                  image={image}
-                  name={name}
-                  body={body}
-                  repo={repo}
-                  live={live}
-                />
+                <motion.div className="carousel-item">
+                  <ProjectCard
+                    key={name}
+                    image={image}
+                    name={name}
+                    body={body}
+                    repo={repo}
+                    live={live}
+                  />
+                </motion.div>
               );
             })}
-        </div>
+          </motion.div>
+        </motion.div>
         <span className="tags">&lt;/projects&gt;</span>
       </section>
     </>
