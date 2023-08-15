da93ea44d3b9ff65acbdb2f3373b86b333f8b5b3
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-15 14:11:11 +0100

    fix dynamic project images imports

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,27 @@
+import pyPad from "../assets/pypadcard.png"
+
 const projects = [
   {
-    image: "./pypadcard.png",
+    image: pyPad,
     name: "PyPad",
     body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
     repo: "https://github.com/The-Nightman/Py-Pad",
     live: null,
   },
   {
-    image: "./pypadcard.png",
+    image: pyPad,
     name: "Pypad",
     body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
     repo: "https://github.com/The-Nightman/Py-Pad",
     live: null,
   },
   {
-    image: "./pypadcard.png",
+    image: pyPad,
     name: "Pypad",
     body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
     repo: "https://github.com/The-Nightman/Py-Pad",
     live: null,
   },
 ];
+
+export { projects };
