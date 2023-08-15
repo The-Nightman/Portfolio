a6184fdf4d4fafcf7b6600444d6df2d2c36d6c3f
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-16 00:07:38 +0100

    add false draggable property to projects cards elements

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as GithubSVG } from "../assets/github.svg";
 const ProjectCard = ({ image, name, body, repo, live }) => {
   return (
     <div className="proj-card">
-      <img src={image} width="250" height="250" />
+      <img src={image} width="250" height="250" draggable="false" />
       <p className="description">
         {name}
         <br />
@@ -15,6 +15,7 @@ const ProjectCard = ({ image, name, body, repo, live }) => {
         href={repo}
         target="_blank"
         className="proj-button"
+        draggable="false"
       >
         <GithubSVG className="project-github" />
         <span>Repository</span>
