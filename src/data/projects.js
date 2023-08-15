68547307bde81d5794d6043326419e0ffe9f599a
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-15 13:45:35 +0100

    convert json to js file for dynamic project data

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    image: "./pypadcard.png",
+    name: "PyPad",
+    body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
+    repo: "https://github.com/The-Nightman/Py-Pad",
+    live: null,
+  },
+  {
+    image: "./pypadcard.png",
+    name: "Pypad",
+    body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
+    repo: "https://github.com/The-Nightman/Py-Pad",
+    live: null,
+  },
+  {
+    image: "./pypadcard.png",
+    name: "Pypad",
+    body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
+    repo: "https://github.com/The-Nightman/Py-Pad",
+    live: null,
+  },
+];
