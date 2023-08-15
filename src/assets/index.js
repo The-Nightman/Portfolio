5b54edfb3a6d342614d10556c8a4b9b3bf007586
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-15 16:22:45 +0100

    add assets svg import/export barrel

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import { ReactComponent as HtmlSVG } from "./html.svg";
+import { ReactComponent as CssSVG } from "./css.svg";
+import { ReactComponent as JavaScriptSVG } from "./javascript.svg";
+import { ReactComponent as ReactSVG } from "./react.svg";
+import { ReactComponent as NodeSVG } from "./node.svg";
+import { ReactComponent as ExpressSVG } from "./express.svg";
+import { ReactComponent as PsqlSVG } from "./psql.svg";
+import { ReactComponent as CvButton } from "./cvbutton.svg";
+import { ReactComponent as GithubSVG } from "./github.svg";
+import { ReactComponent as LinkedinSVG } from "./linkedin.svg";
+
+export {
+  HtmlSVG,
+  CssSVG,
+  JavaScriptSVG,
+  ReactSVG,
+  NodeSVG,
+  ExpressSVG,
+  PsqlSVG,
+  CvButton,
+  GithubSVG,
+  LinkedinSVG,
+};
