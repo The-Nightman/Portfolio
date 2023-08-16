2d859ad58a4fa0a9033ee936a10695aac8844e84
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-08-16 18:49:22 +0100

    add accesibility aria labels

--- src/components/nav-desktop.jsx
+++ src/components/nav-desktop.jsx
@@ -5,25 +5,32 @@ const NavDesktop = () => {
   return (
     <nav className="header-navbar-desktop">
       <ul>
-        <li>
-          <Link to="/" draggable="false">
-            K.dev()
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" draggable="false">
-            .about()
-          </Link>
-        </li>
-        <li>
-          <Link to="#" draggable="false">
-            .email()
-          </Link>
-        </li>
+      <li>
+            <Link to="/" draggable="false" title="Home" aria-label="Home">
+              K.dev()
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              draggable="false"
+              title="About"
+              aria-label="About"
+            >
+              .about()
+            </Link>
+          </li>
+          <li>
+            <Link to="#" draggable="false" title="Email" aria-label="Email">
+              .email()
+            </Link>
+          </li>
         <li className="github-desktop" title="GitHub">
           <Link
             to="https://github.com/The-Nightman"
             target="_blank"
+            title="GitHub"
+            aria-label="GitHub"
             draggable="false"
           >
             <GithubSVG />
@@ -32,6 +39,8 @@ const NavDesktop = () => {
         <li className="linkedin-desktop" title="LinkedIn">
           <Link
             to="https://www.linkedin.com/in/keenan-brant/"
+            title="Linked in"
+            aria-label="Linked in"
             target="_blank"
             draggable="false"
           >
--- src/components/nav-mobile.jsx
+++ src/components/nav-mobile.jsx
@@ -7,23 +7,42 @@ const NavMobile = () => {
       <ul>
         <div className="navbar-mobile">
           <li>
-            <Link to="/" draggable="false">K.dev()</Link>
+            <Link to="/" draggable="false" title="Home" aria-label="Home">
+              K.dev()
+            </Link>
           </li>
           <li>
-            <Link to="/about" draggable="false">.about()</Link>
+            <Link
+              to="/about"
+              draggable="false"
+              title="About"
+              aria-label="About"
+            >
+              .about()
+            </Link>
           </li>
           <li>
-            <Link to="#" draggable="false">.email()</Link>
+            <Link to="#" draggable="false" title="Email" aria-label="Email">
+              .email()
+            </Link>
           </li>
           <div className="navbar-icons-mobile">
             <li className="github-mobile" title="GitHub">
-              <Link to="https://github.com/The-Nightman" target="_blank" draggable="false">
+              <Link
+                to="https://github.com/The-Nightman"
+                target="_blank"
+                title="GitHub"
+                aria-label="GitHub"
+                draggable="false"
+              >
                 <GithubSVG />
               </Link>
             </li>
             <li className="linkedin-mobile" title="LinkedIn">
               <Link
                 to="https://www.linkedin.com/in/keenan-brant/"
+                title="Linked in"
+                aria-label="Linked in"
                 target="_blank"
                 draggable="false"
               >
