f41b72a0f767fe85d90eaa134913f01cd56376a5
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-04 23:19:41 +0100

    migrate IntroHome to tailwind styles and accessibility improvements

--- src/components/intro-home.jsx
+++ src/components/intro-home.jsx
@@ -1,19 +1,25 @@
 const IntroHome = () => {
   return (
     <>
-      <section className="personal-intro">
-        <span className="tags">&lt;intro&gt;</span>
-        <h1 className="intro-title">
-          <span className="keenan">KBrant</span>
-          <span className="func">.dev()</span>
+      <section className="flex flex-col min-h-[60vh]">
+        <span className="my-16 text-accent" aria-hidden>
+          {"<intro>"}
+        </span>
+        <h1 className="text-5xl font-bold text-primary">
+          KBrant
+          <span className="text-accent">
+            .dev<span aria-hidden>()</span>
+          </span>
         </h1>
-        <p className="name">Keenan Brant</p>
+        <p className="text-[2rem]/[1.2] text-accent">Keenan Brant</p>
         <p>
           Junior Software Developer
           <br />
           based in the UK
         </p>
-        <span className="tags">&lt;/intro&gt;</span>
+        <span className="text-accent my-16" aria-hidden>
+          {"</intro>"}
+        </span>
       </section>
     </>
   );
