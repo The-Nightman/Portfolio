f5c47bc125254e424574d244381c1a800cdbf891
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-09 02:19:39 +0100

    migrate AboutSkills to tailwind styles and accessibility improvements

--- src/components/about-skills.jsx
+++ src/components/about-skills.jsx
@@ -1,23 +1,27 @@
 const AboutSkills = () => {
   return (
     <>
-      <section className="about-skills">
-        <h2 className="about-skills-title">Skills</h2>
-        <ul className="skills-grid">
+      <section className="flex flex-col mb-8">
+        <h2 className="mb-4 text-2xl font-bold">Skills</h2>
+        <ul className="grid w-11/12 [grid-template: 1fr / 1fr] sm:grid-cols-3 gap-4 sm:gap-8">
           <li>
             <p>
-              /* HTML, CSS, Tailwind, JavaScript, Node, Express, React, React
-              Native, C#, .NET 7, ASP.NET core, SQL, PostgreSQL, Python, SVG */
+              <span aria-hidden>/*</span> HTML, CSS, Tailwind, JavaScript, Node,
+              Express, React, React Native, C#, .NET 7, ASP.NET core, SQL,
+              PostgreSQL, Python, SVG <span aria-hidden>*/</span>
             </p>
           </li>
           <li>
             <p>
-              /* TDD (Jest, Swagger, Insomnia), MVC, RESTful API, GitHub
-              Actions, UX/UI */
+              <span aria-hidden>/*</span> TDD (Jest, Swagger, Insomnia), MVC,
+              RESTful API, GitHub Actions, UX/UI <span aria-hidden>*/</span>
             </p>
           </li>
           <li>
-            <p>/* Adobe Illustrator, Adobe Photoshop, Adobe Xd, Figma */</p>
+            <p>
+              <span aria-hidden>/*</span> Adobe Illustrator, Adobe Photoshop,
+              Adobe Xd, Figma <span aria-hidden>*/</span>
+            </p>
           </li>
         </ul>
       </section>
