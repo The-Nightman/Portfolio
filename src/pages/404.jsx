2f85873545daea60658723ca43ae71461e28b245
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-09 02:55:59 +0100

    migrate Error to tailwind styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,22 +5,26 @@ const Error = () => {
   return (
     <>
       <Navbar />
-      <div className="separator" />
-      <div className="error">
-        <div className="error-container">
-          <div className="error-text">
-            <h1 className="error-header">404</h1>
-            <p className="error-subheader">What are you looking for?</p>
-            <p className="error-flavour">
-              I don't know how or why you're here,
-              <br />
-              but before you go i just want you to know,
-              <br />
-              <span>you are...</span>
-            </p>
-          </div>
-          <img className="error-image" src={errorImg} draggable="false" alt="You are Kenough" />
+      <div className="absolute flex flex-col min-h-[calc(100%-(9vh+4rem))] items-center justify-between">
+        <div className="flex flex-col text-center">
+          <h1 className="text-accent text-9xl font-bold">404</h1>
+          <p className="mb-4 text-primary text-[1.4rem]/[1.75rem] sm:text-[2rem]/[2.5rem] font-medium">
+            What are you looking for?
+          </p>
+          <p className="mb-12 sm:mb-0 text-xl">
+            I don't know how or why you're here,
+            <br />
+            but before you go i just want you to know,
+            <br />
+            <span className="text-accentComplementary">you are...</span>
+          </p>
         </div>
+        <img
+          className="w-full sm:w-[40%] lg:w-[30%]"
+          src={errorImg}
+          draggable="false"
+          alt="You are Kenough"
+        />
       </div>
     </>
   );
