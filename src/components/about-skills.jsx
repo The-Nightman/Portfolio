83f21118019f464363ac33d436f6c50356c4be8b
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-09 18:16:10 +0100

    update AboutSkills content

--- src/components/about-skills.jsx
+++ src/components/about-skills.jsx
@@ -3,18 +3,20 @@ const AboutSkills = () => {
     <>
       <section className="flex flex-col mb-8">
         <h2 className="mb-4 text-2xl font-bold">Skills</h2>
-        <ul className="grid w-11/12 [grid-template: 1fr / 1fr] sm:grid-cols-3 gap-4 sm:gap-8">
+        <ul className="grid w-11/12 md:w-full [grid-template: 1fr / 1fr] sm:grid-cols-3 gap-4 sm:gap-8">
           <li>
             <p>
-              <span aria-hidden>/*</span> HTML, CSS, Tailwind, JavaScript, Node,
-              Express, React, React Native, C#, .NET 7, ASP.NET core, SQL,
-              PostgreSQL, Python, SVG <span aria-hidden>*/</span>
+              <span aria-hidden>/*</span> HTML, CSS, Tailwind, JavaScript,
+              TypeScript, Node, Express, React, React Native, C#, .NET 7,
+              ASP.NET core, SQL, PostgreSQL, Entity Framework Core, Python, SVG{" "}
+              <span aria-hidden>*/</span>
             </p>
           </li>
           <li>
             <p>
               <span aria-hidden>/*</span> TDD (Jest, Swagger, Insomnia), MVC,
-              RESTful API, GitHub Actions, UX/UI <span aria-hidden>*/</span>
+              RESTful API, UX/UI, GitHub Actions, Docker{" "}
+              <span aria-hidden>*/</span>
             </p>
           </li>
           <li>
