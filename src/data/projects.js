f25c3df5cafebd69143cf7fe3abb7dfa7bdc2683
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-09-23 01:25:24 +0100

    replace ed voting json builder in projects data and image with new ed resurgence json builder

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,10 @@
-import pyPad from "../assets/pypadcard.png"
-import silba from "../assets/silba.png"
-import NCNewsFE from "../assets/NCNewsFE.png"
-import NCNewsBE from "../assets/NCNewsBE.png"
-import edJsonBuilder from "../assets/edjsonbuilder.png"
-import IBDirect from "../assets/IBDirect.png"
-import AtomiFit from "../assets/atomifit.png"
+import pyPad from "../assets/pypadcard.png";
+import silba from "../assets/silba.png";
+import NCNewsFE from "../assets/NCNewsFE.png";
+import NCNewsBE from "../assets/NCNewsBE.png";
+import edrJsonBuilder from "../assets/edrjsonbuilder.png";
+import IBDirect from "../assets/IBDirect.png";
+import AtomiFit from "../assets/atomifit.png";
 
 const projects = [
   {
@@ -13,31 +13,37 @@ const projects = [
     body: "WIP cross-platform fitness tracker app. Planned as a freemium app with features to bridge the gap in the current market such as: detailed analytics, out of the box programs, ML object detection for bar path tracking. Built with React Native w/TS, DrizzleORM and native libraries incl. SQLite.",
     repo: "https://github.com/The-Nightman/AtomiFit",
     live: null,
-    fullstack: false
+    fullstack: false,
   },
   {
     image: IBDirect,
     name: "IBDirect",
     body: "NHS based web-portal for IBD patients and healthcare specialists to monitor, update and create records & appointments. Responsive design for devices used by NHS staff and patients with an ASP.NET API using EFCore and SignalR.",
-    repo: ["https://github.com/The-Nightman/IBDirect","https://github.com/The-Nightman/IBDirectAPI"],
+    repo: [
+      "https://github.com/The-Nightman/IBDirect",
+      "https://github.com/The-Nightman/IBDirectAPI",
+    ],
     live: "https://ibdirect.net/",
-    fullstack: true
+    fullstack: true,
   },
   {
     image: silba,
     name: "SILBA",
     body: "SILBA is a react-native mobile app platform for small businesses to advertise or provide goods and services with a sleek and intuitive UI. Created as part of a FullStack group project at Northcoders.",
-    repo: ["https://github.com/JoeAshcroft/SILBA","https://github.com/Kayphaz007/SILBA_BE"],
+    repo: [
+      "https://github.com/JoeAshcroft/SILBA",
+      "https://github.com/Kayphaz007/SILBA_BE",
+    ],
     live: null,
-    fullstack: true
+    fullstack: true,
   },
   {
-    image: edJsonBuilder,
-    name: "ElDewrito Json Builder",
-    body: "An Electron based production app with an accessible UI/UX focus and automation, built to help server hosts build the required voting.json file to host servers. Actively maintained with GHub actions for CI/CD pipeline.",
-    repo: "https://github.com/The-Nightman/ED-VotingJSON",
+    image: edrJsonBuilder,
+    name: "ElDewrito Resurgence Json Builder",
+    body: "An Electron production app with an accessible UI/UX focus and automation, built to help server hosts build the required voting.json and mods.json files to host servers. Actively maintained with GHub actions for CI/CD pipeline.",
+    repo: "https://github.com/The-Nightman/EDResurgence-VotingJSON-Builder",
     live: null,
-    fullstack: false
+    fullstack: false,
   },
   {
     image: NCNewsFE,
@@ -45,7 +51,7 @@ const projects = [
     body: "NC News is an interactive responsive mobile-first single-page webapp using functional components and hooks made with react, JS, MUI, and CSS connected to a RESTful API backend.",
     repo: "https://github.com/The-Nightman/NC-News",
     live: "https://ncnewsfeproject.netlify.app/",
-    fullstack: false
+    fullstack: false,
   },
   {
     image: NCNewsBE,
@@ -53,7 +59,7 @@ const projects = [
     body: "Backend RESTful API built with NodeJS and Express capable of receiving CRUD requests and returning data through multiple endpoints with built in documentation.",
     repo: "https://github.com/The-Nightman/be-nc-news",
     live: "https://nc-news-v54w.onrender.com/api",
-    fullstack: false
+    fullstack: false,
   },
   {
     image: pyPad,
@@ -61,7 +67,7 @@ const projects = [
     body: "Notepad clone written in python as a gui app using elements from tkinter and customtkinter to create a familiar but smooth interface.",
     repo: "https://github.com/The-Nightman/Py-Pad",
     live: null,
-    fullstack: false
+    fullstack: false,
   },
 ];
 
